8273ba78ed36b2aa12e646874b299038b3c8ea9a
David Santomé <61311785+SGDavid2719@users.noreply.github.com>
2021-12-11 07:57:54 +0100

    Add files via upload
    Project init

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DreamHome Login</title>
+</head>
+<body>
+<header>
+        <div class="header m-4 pt-2">
+            <h1>DreamHome</h1>
+            <hr class="headerSplitter" />
+        </div>
+    </header>
+    <section>
+        <div class="demo-container">
+            <form action="your-action" id="form-container">
+                <div id="form"></div>
+            </form>
+        </div>
+    </section>
+    <footer>
+        <div class="footer">
+            &copy; 2021 David Santomé & Raixa Madueño
+        </div>
+    </footer>
+</body>
+</html>
