57ce2c3e1564c5a169322f402e341b7bcb764ae3
SGDavid2719 <santome.galvan.david@gmail.com>
2021-12-11 08:11:17 +0100

    html form

--- login.php
+++ login.php
@@ -11,16 +11,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 </head>
 <body>
-<header>
+    <header>
         <div class="header m-4 pt-2">
             <h1>DreamHome</h1>
             <hr class="headerSplitter" />
         </div>
     </header>
     <section>
-        <div class="demo-container">
-            <form action="your-action" id="form-container">
-                <div id="form"></div>
+        <div class="container">
+            <form>
+                <label for="fname">Email Address:</label><br>
+                <input type="text" id="fname" name="fname" value=""><br>
+                <label for="lname">Password:</label><br>
+                <input type="text" id="lname" name="lname" value="">
+                <input type="submit" value="Submit">
             </form>
         </div>
     </section>
