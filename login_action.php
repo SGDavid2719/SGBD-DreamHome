b4b4c89f9c34fc7a9c6fa254f9db5d2d2698e20a
SGDavid2719 <santome.galvan.david@gmail.com>
2021-12-11 11:01:52 +0100

    login test through mysql db

--- login_action.php
+++ login_action.php
@@ -4,5 +4,37 @@
         header('Location: ' . $url, true, $permanent ? 301 : 302);
         exit();
     }
-    Redirect("index.php", false);
+
+    $user_address=$_POST['eaddress'];
+    $user_password=$_POST['password'];
+    //start_session();
+    $_SESSION['eaddress']=$user_address;
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "credentials";
+    // Create connection
+    $connection = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    $query="SELECT*FROM Users where userAddress='$user_address' and pass='$user_password'";
+    $result=mysqli_query($connection, $query);
+
+    $rows=mysqli_num_rows($result);
+    if($rows) {
+        Redirect('index.php', false);
+    } else {
+        ?>
+        <?php
+            include("login.php");
+        ?>
+        <h1 class="bad">ERROR</H1>
+        <?php
+    }
+    mysqli_free_result($result);
+    mysqli_close($connection);
 ?>
