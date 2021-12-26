86de3e0087b8534fe5287548514acc5451a79a7d
SGDavid2719 <santome.galvan.david@gmail.com>
2021-12-26 16:55:33 +0100

    sql file and get data function

--- php/src/utilities.php
+++ php/src/utilities.php
@@ -4,4 +4,34 @@
         header('Location: ' . $url, true, $permanent ? 301 : 302);
         exit();
     }
+
+    function GetData($pTable)
+    {
+        // Connection arguments
+        $host        = "host = postgresdb";
+        $port        = "port = 5432";
+        $dbname      = "dbname = POSTGRES_DB";
+        $credentials = "user = POSTGRES_USER password=POSTGRES_PASSWORD";
+
+        // Create connection
+        $connection = pg_connect( "$host $port $dbname $credentials");
+        // Check connection
+        if(!$connection) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+        $query="SELECT*FROM $pTable";
+        $result = pg_query($connection, $query);
+    
+        $data = pg_fetch_row($result);
+        $rows = pg_num_rows($result);
+    
+        if($rows) {
+            return $data;
+        } else {
+            // Error
+        }
+        pg_free_result($result);
+        pg_close($connection);
+    }
 ?>
